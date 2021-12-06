d63fd8b686feaff3b4e688452a83e9b6bd2280eb
Mike Malashkin <mike@MacBook-Pro-Mikhail.local>
2021-12-06 12:36:48 +0000

    complete4

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
 const edit = document.querySelector('.profile__button');
 const show = document.querySelector('.popup');
 const closeModal = document.querySelector('.popup__close');
-
+let formElement = document.querySelector('.popup__container');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__spec');
 
 function openModal() {
     show.classList.remove('popup');
@@ -13,19 +15,15 @@ function close() {
     show.classList.add('popup');
 }
 
-edit.addEventListener('click', openModal);
-closeModal.addEventListener('click', close);
-let formElement = document.querySelector('.popup__container');
-
 function submitFormHandler(evt) {
     evt.preventDefault();
-    let profileName = document.querySelector('.profile__name');
-    let profileJob = document.querySelector('.profile__spec');
-    let nameInput = document.querySelector('.popup__text-name').value;
-    let jobInput = document.querySelector('.popup__text-spec').value;
+    const nameInput = document.querySelector('.popup__text-name').value;
+    const jobInput = document.querySelector('.popup__text-spec').value;
     profileName.textContent = nameInput;
     profileJob.textContent = jobInput;
     close();
 }
 
+edit.addEventListener('click', openModal);
+closeModal.addEventListener('click', close);
 formElement.addEventListener('submit', submitFormHandler);
