97c08d5ada40f1ebb1a6944c0cc7b487f12cbc7c
Mike Malashkin <mike@MacBook-Pro-Mikhail.local>
2021-12-07 12:28:56 +0000

    complete7

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,17 @@
 const edit = document.querySelector('.profile__button');
 const show = document.querySelector('.popup');
-const closeModal = document.querySelector('.popup__close');
-let formElement = document.querySelector('.popup__container');
-let profileName = document.querySelector('.profile__name');
-let profileJob = document.querySelector('.profile__spec');
+const closeModal = show.querySelector('.popup__close');
+const formElement = document.querySelector('.popup__container');
+const profileName = document.querySelector('.profile__name');
+const profileJob = document.querySelector('.profile__spec');
+const nameInput = document.querySelector('.popup__text_type_name');
+const jobInput = document.querySelector('.popup__text_type_job');
+
 
 function openModal() {
     show.classList.remove('popup');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
 }
 
 function close() {
@@ -15,10 +20,8 @@ function close() {
 
 function submitFormHandler(evt) {
     evt.preventDefault();
-    const nameInput = document.querySelector('.popup__text-name').value;
-    const jobInput = document.querySelector('.popup__text-spec').value;
-    profileName.textContent = nameInput;
-    profileJob.textContent = jobInput;
+    profileName.textContent = nameInput.value;
+    profileJob.textContent = jobInput.value;
     close();
 }
 
