c6e03820877df9f3b3797c16180b5c5456103fa0
Mike Malashkin <mike@MacBook-Pro-Mikhail.local>
2021-12-26 10:49:01 +0000

    Added function of delete

--- scripts/script.js
+++ scripts/script.js
@@ -6,19 +6,19 @@ const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__spec');
 const nameInput = document.querySelector('.popup__text_type_name');
 const jobInput = document.querySelector('.popup__text_type_job');
-const formContainer = document.querySelector('.cards')
-const addButton = document.querySelector('.profile__add')
-const addButtonSave = document.getElementById('saveAdded')
-const addModal = document.getElementById('popupAdd')
-const cards = document.querySelector('.cards')
-const submitAdd = document.getElementById('saveAdded')
+const formContainer = document.querySelector('.cards');
+const addButton = document.querySelector('.profile__add');
+const addButtonSave = document.getElementById('saveAdded');
+const addModal = document.getElementById('popupAdd');
+const cards = document.querySelector('.cards');
+const submitAdd = document.getElementById('saveAdded');
 const initialContent = document.querySelector('.cards__list').content;
 const initialCardLink = document.querySelector('.cards__image');
 const initialCardTitle = document.querySelector('.cards__title');
 const closeAddModal = document.getElementById('addClose');
 const ItemImage = document.querySelector('.popup__text_type_image');
 const ItemTitle = document.querySelector('.popup__text_type_title');
-const formAdd = document.querySelector('.popup__container_type_add')
+const formAdd = document.querySelector('.popup__container_type_add');
 const initialCards = [{
         name: 'Архыз',
         link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
@@ -47,58 +47,80 @@ const initialCards = [{
 
 initialCards.forEach(function(card) {
     initialData(card)
-})
+});
 
 function initialData(card) {
     const initialCard = initialContent.querySelector('.cards__conteiner').cloneNode(true);
+    const initialLike = initialCard.querySelector('.cards__emotion');
+    const initialDelete = initialCard.querySelector('.cards__trashicon')
     initialCard.querySelector('.cards__image').src = card.link
     initialCard.querySelector('.cards__title').textContent = card.name;
+    initialCard.querySelector('.cards__image').alt = card.name;
+    initialLike.addEventListener('click', function clickLike() {
+        initialLike.classList.toggle('cards__emotion_active')
+    });
+    initialDelete.addEventListener('click', function initialDel() {
+        initialDelete.closest('.cards__conteiner').remove();
+    })
     cards.prepend(initialCard)
-}
+};
+
+
 
 function submitFormHandlerAdd(evt) {
     evt.preventDefault();
     const newItem = initialContent.querySelector('.cards__conteiner').cloneNode(true);
+    const likeButton = newItem.querySelector('.cards__emotion');
+    const newItemDelete = newItem.querySelector('.cards__trashicon')
     newItem.querySelector('.cards__title').textContent = ItemTitle.value;
     newItem.querySelector('.cards__image').src = ItemImage.value;
     newItem.querySelector('.cards__image').alt = ItemTitle.value;
-    cards.prepend(newItem)
-    formAdd.reset()
+    likeButton.addEventListener('click', function newLike() {
+        likeButton.classList.toggle('cards__emotion_active')
+    });
+    newItemDelete.addEventListener('click', function newItemDel() {
+        newItemDelete.closest('.cards__conteiner').remove();
+    })
+    cards.prepend(newItem);
+    formAdd.reset();
     closeAddedModal()
-    console.log({ newItem });
-}
+};
 
-
-function openEditModal() {
-    show.classList.remove('popup');
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-}
-
-function closeEditModal() {
-    show.classList.add('popup');
-}
 
 function submitFormHandlerEdit(evt) {
     evt.preventDefault();
     profileName.textContent = nameInput.value;
     profileJob.textContent = jobInput.value;
     closeEditModal();
-}
+};
+
+function openEditModal() {
+    show.classList.remove('popup');
+    nameInput.value = profileName.textContent;
+    jobInput.value = profileJob.textContent;
+};
+
+function closeEditModal() {
+    show.classList.add('popup');
+};
 
 function openAddModal() {
     addModal.classList.remove('popup');
-}
+};
 
 function closeAddedModal() {
     addModal.classList.add('popup');
+};
+
+
+
+function clickLike() {
+    console.log('fuck');
 }
-
-
 
 editButton.addEventListener('click', openEditModal);
 closeModal.addEventListener('click', closeEditModal);
 formEdit.addEventListener('submit', submitFormHandlerEdit);
 addButton.addEventListener('click', openAddModal);
 closeAddModal.addEventListener('click', closeAddedModal);
-formAdd.addEventListener('submit', submitFormHandlerAdd)
+formAdd.addEventListener('submit', submitFormHandlerAdd);
