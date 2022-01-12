a18e1a0e5472d8c31f55b3fea0f369e0586627aa
Mike Malashkin <mike@MacBook-Pro-Mikhail.local>
2022-01-12 16:32:58 +0000

    validation of 1 input

--- scripts/validate.js
+++ scripts/validate.js
@@ -3,6 +3,62 @@ const valitateOption = {
     inputSelector: '.popup__input',
     submitButtonSelector: '.popup__button',
     inactiveButtonClass: 'popup__button_disabled',
-    inputErrorClass: 'popup__input_type_error',
-    errorClass: 'popup__error_visible'
+    errorClass: 'popup__error_visible', // полоска 
+    inputErrorClass: 'popup__input_type_error', // Текст
+
 };
+
+const formElement = document.querySelector('.popup__form');
+const formInput = formElement.querySelector('.popup__input');
+const formError = formElement.querySelector(`.${formInput.id}-error`);
+
+
+const showInputError = (element, errorMessage) => {
+    element.classList.add('popup__error_visible');
+    formError.textContent = errorMessage;
+    formError.classList.add('popup__input_type_error');
+}
+
+const hideInputError = (element) => {
+    element.classList.remove('popup__error_visible')
+    formError.textContent = ''
+    formError.classList.remove('popup__input_type_error')
+};
+
+const isValid = () => {
+        if (!formInput.validity.valid) {
+            showInputError(formInput, formInput.validationMessage);
+        } else {
+            hideInputError(formInput)
+        }
+    }
+    // Не перебирает все поля инпута
+const hasInvalidInput = (inputList) => {
+    return inputList.some((inputElement) => {
+        return !inputElement.validity.valid
+    })
+}
+const toggleButtonState = (inputList, buttonElement) => {
+    if (hasInvalidInput(inputList)) {
+        buttonElement.classList.add('popup__button_disabled')
+    } else {
+        buttonElement.classList.remove('popup__button_disabled')
+    }
+}
+
+const setEventListeners = (formElement) => {
+    const inputList = Array.from(formElement.querySelectorAll('.popup__input'));
+    const buttonElement = formElement.querySelector('.popup__button');
+    inputList.forEach((inputElement) => {
+        inputElement.addEventListener('input', () => {
+            isValid(formElement, inputElement)
+            toggleButtonState(inputList, buttonElement)
+        })
+    })
+}
+
+formInput.addEventListener('submit', (evt) => {
+    evt.preventDefailt()
+});
+
+formElement.addEventListener('input', isValid)
