2dea0a02bd31bd58b3af9ef867d6f6295deba168
Mike Malashkin <mike@MacBook-Pro-Mikhail.local>
2022-02-11 14:05:45 +0000

    удалена неактуальная часть

--- src/pages/index.js
+++ src/pages/index.js
@@ -25,110 +25,41 @@ const initialItems = new Section({
 }, '.cards');
 
 
-initialCards.forEach((data) => {
-    addNewCard(createNewCard(data))
-});
-
 function createNewCard(data) {
     const newCard = new Card(data, openPopupZoom, '.cards__list')
     return newCard.generateCard()
 };
 
-function addNewCard(card) { // перенесена в Section
-    cards.prepend(card);
-};
 
-/*
+
+
 const newUserInfo = new UserInfo('.profile__name', '.profile__spec')
-  
-    function setInputValue() {
-        nameInput.value = newUserInfo.getUserInfo().name;
-        jobInput.value = newUserInfo.getUserInfo().info;
-    }
 
-    function setUserInfo(data) {
-        newUserInfo.setUserInfo(data)
-    };
-    console.log();
-    const popupFormEdit = new PopupWithForm('popup__form_type_edit', (data) => {
-        setUserInfo(data)
-    });
-    editButton.addEventListener('click', () => {
-        setInputValue();
-        popupFormEdit.open();
-    })
-
-    popupFormEdit.setEventListeners();
-    */
-
-
-
-
-
-function submitFormHandlerAdd(evt) { // перенесено
-    evt.preventDefault();
-    const newItem = {
-        link: itemImage.value,
-        name: itemTitle.value,
-    }
-    addNewCard(createNewCard(newItem))
-    closePopup(popupAdd)
-    formAdd.reset();
-    addFormValidation.deactivetingSubmit();
-};
-
-function submitFormHandlerEdit(evt) { // перенесено
-    evt.preventDefault();
-    profileName.textContent = nameInput.value;
-    profileJob.textContent = jobInput.value;
-    formEdit.reset()
-    closePopup(popupEdit)
-};
-
-function openEditModal() { //
-    openPopup(popupEdit);
-    nameInput.value = profileName.textContent;
-    jobInput.value = profileJob.textContent;
-};
-
-function openPopupZoom(link, name) {
-    openPopup(popupZoom);
-    popupImage.src = link;
-    popupImage.alt = name;
-    popupSubtitle.textContent = name;
-};
-
-function openPopup(popup) { // перенесено в Popup
-    popup.classList.add('popup_type_opened');
-    document.addEventListener('mousedown', closeByOverlay)
-    document.addEventListener('keydown', closeByEscapeButton)
-};
-
-function closePopup(popup) { // перенесено в Popup
-    popup.classList.remove('popup_type_opened')
-    document.removeEventListener('mousedown', closeByOverlay)
-    document.removeEventListener('keydown', closeByEscapeButton)
+function setInputValue() {
+    nameInput.value = newUserInfo.getUserInfo().name;
+    jobInput.value = newUserInfo.getUserInfo().info;
 }
 
-function closeByOverlay(evt) { // перенесено в Popup
-    const overlay = document.querySelector('.popup_type_opened')
-    if (evt.target === overlay) {
-        closePopup(overlay)
-        formEdit.reset()
-    }
-}
+function setUserInfo(data) {
+    newUserInfo.setUserInfo(data)
+};
+console.log();
+const popupFormEdit = new PopupWithForm('popup__form_type_edit', (data) => {
+    setUserInfo(data)
+});
+editButton.addEventListener('click', () => {
+    setInputValue();
+    popupFormEdit.open();
+})
 
-function closeByEscapeButton(evt) { // перенесено в Popup
-    const overlay = document.querySelector('.popup_type_opened')
-    if (evt.key === 'Escape') {
-        closePopup(overlay)
-    }
-}
+popupFormEdit.setEventListeners();
 
-buttonClosePopupAdd.addEventListener('click', () => closePopup(popupAdd));
-buttonClosePopupEdit.addEventListener('click', () => closePopup(popupEdit));
-buttonClosePopupZoom.addEventListener('click', () => closePopup(popupZoom));
-editButton.addEventListener('click', openEditModal);
-formEdit.addEventListener('submit', submitFormHandlerEdit);
-addButton.addEventListener('click', () => openPopup(popupAdd));
-formAdd.addEventListener('submit', submitFormHandlerAdd);
+const popupFormAdd = new PopupWithForm('.popup__form_type_add', (data) => {
+    cards.prepend(generateCard(data))
+})
+
+addButton.addEventListener('click', () => {
+    popupFormAdd.open()
+});
+
+popupFormAdd.setEventListeners()
